47c27102fb76907eff6338c74e31249b594944de
Helen <helenmou666@gmail.com>
2018-07-27 16:22:55 +1000

    fetch data from url

---
+++ react-mix/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ParisWeather from './ParisWeather';
+import './styles/app.css';
+
+ReactDOM.render(<ParisWeather />, document.getElementById('root'));
