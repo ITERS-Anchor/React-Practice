a54c1c5996671378ed919de4fda89077dbff6fcc
Helen <helenmou666@gmail.com>
2018-07-27 20:03:52 +1000

    Add search textbox and button

--- react-mix/src/Async-cdnjsAPI.jsx
+++ react-mix/src/Async-cdnjsAPI.jsx
@@ -4,27 +4,42 @@ export default class CDNJS extends React.Component {
   constructor() {
     super();
     this.state = {
+      userinput: 'jquery',
       data: null,
       isLoading: false,
     };
+    this.SearchHandler = this.SearchHandler.bind(this);
+    this.InputOnchangeHandler = this.InputOnchangeHandler.bind(this);
   }
   componentDidMount() {
     this.fetchData();
   }
   fetchData() {
-    const url = 'https://api.cdnjs.com/libraries/jquery';
+    const url = `https://api.cdnjs.com/libraries/${this.state.userinput}`;
     $.getJSON(url, (response) => {
       this.setState({ data: response });
+    });
+  }
+  InputOnchangeHandler(e) {
+    this.setState({
+      userinput: e.target.value,
+    });
+  }
+  SearchHandler(e) {
+    e.preventDefault();
+    this.fetchData();
+    this.setState({
+      userinput: '',
     });
   }
   renderData() {
     const { data } = this.state;
     return (
       <div>
-        <label>Name</label> {data.name} <br />
-        <label>File name</label> {data.filename} <br />
-        <label>Version</label> {data.version} <br />
-        <label>License</label> {data.license} <br />
+        <label>Name:</label> {data.name} <br />
+        <label>File name:</label> {data.filename} <br />
+        <label>Version:</label> {data.version} <br />
+        <label>License:</label> {data.license} <br />
         <label>All versions</label>
         <div>
           {data.assets.map(x => (<div>{x.version}</div>))}
@@ -36,7 +51,15 @@ export default class CDNJS extends React.Component {
   render() {
     return (
       <div className="container" style={{ margin: '15px auto' }}>
-        <h3> jQuery Versions</h3>
+        <form onSubmit={this.SearchHandler}>
+          <input
+            value={this.state.userinput}
+            onChange={this.InputOnchangeHandler}
+            placeholder="Enter search item"
+          />
+          <button type="submit" onClick={this.SearchHandler}>Search</button>
+        </form>
+        <h3>CDNJS api Versions</h3>
         {this.state.data && this.renderData()}
       </div>
 
